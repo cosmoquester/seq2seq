04b280cb1ca68dedf2355b1293f5135645f1aa64
박상준 <psj8252@naver.com>
2020-12-12 02:11:09 +0900

    test: Add test for dropout

--- tests/test_model.py
+++ tests/test_model.py
@@ -17,17 +17,18 @@ def test_model():
         hidden_dim=hidden_dim,
         num_encoder_layers=2,
         num_decoder_layers=2,
-        dropout=0.0,
+        dropout=0.15,
         use_bidirectional=True,
     )
-    output = model(
-        (
-            tf.random.uniform((batch_size, encoder_sequence), maxval=vocab_size),
-            tf.random.uniform((batch_size, decoder_sequence), maxval=vocab_size),
-        )
+    inputs = (
+        tf.random.uniform((batch_size, encoder_sequence), maxval=vocab_size),
+        tf.random.uniform((batch_size, decoder_sequence), maxval=vocab_size),
     )
+    output = model(inputs)
+    output_with_dropout = model(inputs, training=True)
 
     tf.debugging.assert_equal(tf.shape(output), (batch_size, vocab_size))
+    tf.debugging.assert_none_equal(output, output_with_dropout)
 
     with pytest.raises(Exception):
         model(tf.constant([[30]]))
