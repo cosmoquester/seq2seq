ec9a98a870f5359addbf2ba2f5fe5c53f416281a
psj8252 <psj8252@naver.com>
2021-03-20 01:07:27 +0900

    feat: Add TFRecord data functions

--- seq2seq/data.py
+++ seq2seq/data.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import tensorflow as tf
 import tensorflow_text as text
 
@@ -15,28 +17,65 @@ def get_dataset(dataset_file_path: str, tokenizer: text.SentencepieceTokenizer, 
     """
 
     @tf.function
-    def mapping_fn(text1, text2):
+    def tokenize_fn(source_text, target_text):
         # Tokenize & Add bos, eos
-        token1 = tokenizer.tokenize(text1)
-        token2 = tokenizer.tokenize(text2)
-
-        # Make training example
-        num_examples = tf.shape(token2)[0] - 1
-
-        # [NumExamples, EncoderSequence]
-        encoder_input = tf.repeat([token1], repeats=[num_examples], axis=0)
-        # [NumExamples, DecoderSequence]
-        decoder_input = token2 * tf.sequence_mask(tf.range(1, num_examples + 1), num_examples + 1, tf.int32)
-        # [NumExamples]
-        labels = token2[1:]
-
-        return (encoder_input, decoder_input), labels
+        source_tokens = tokenizer.tokenize(source_text)
+        target_tokens = tokenizer.tokenize(target_text)
+        return source_tokens, target_tokens
 
     if auto_encoding:
-        dataset = tf.data.TextLineDataset(dataset_file_path, num_parallel_reads=tf.data.experimental.AUTOTUNE).map(
-            lambda text: (text, text)
-        )
+        dataset = tf.data.TextLineDataset(
+            dataset_file_path,
+            num_parallel_reads=tf.data.experimental.AUTOTUNE,
+        ).map(lambda text: (text, text))
     else:
         dataset = tf.data.experimental.CsvDataset(dataset_file_path, [tf.string, tf.string], field_delim="\t")
 
-    return dataset.map(mapping_fn)
+    return dataset.map(tokenize_fn).map(make_train_examples)
+
+
+def get_tfrecord_dataset(dataset_file_path: str) -> tf.data.Dataset:
+    """ Read TFRecord dataset file and construct tensorflow dataset """
+    dataset = tf.data.TFRecordDataset(dataset_file_path)
+
+    feature_description = {
+        "source": tf.io.VarLenFeature(tf.int64),
+        "target": tf.io.VarLenFeature(tf.int64),
+    }
+
+    def _parse_fn(example_proto):
+        """ Parse the input `tf.train.Example` proto using the dictionary above. """
+        parsed_example = tf.io.parse_single_example(example_proto, feature_description)
+        source_tokens = tf.squeeze(parsed_example["source"].indices, axis=-1)
+        target_tokens = tf.squeeze(parsed_example["target"].indices, axis=-1)
+        return source_tokens, target_tokens
+
+    return dataset.map(_parse_fn)
+
+
+@tf.function
+def make_train_examples(source_tokens: tf.Tensor, target_tokens: tf.Tensor):
+    """ Make training examples from source and target tokens. """
+    # Make training example
+    num_examples = tf.shape(target_tokens)[0] - 1
+
+    # [NumExamples, EncoderSequence]
+    encoder_input = tf.repeat([source_tokens], repeats=[num_examples], axis=0)
+    # [NumExamples, DecoderSequence]
+    decoder_input = target_tokens * tf.sequence_mask(tf.range(1, num_examples + 1), num_examples + 1, tf.int32)
+    # [NumExamples]
+    labels = target_tokens[1:]
+
+    return (encoder_input, decoder_input), labels
+
+
+def _serialize_example(source_tokens: List[int], target_tokens: List[int]) -> bytes:
+    """ Creates a tf.train.Example message ready to be written to a file. """
+    feature = {"source": _token_feature(source_tokens), "target": _token_feature(target_tokens)}
+    example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+    return example_proto.SerializeToString()
+
+
+def _token_feature(tokens: List[int]) -> tf.train.Feature:
+    """ Make IntList feature from token indices """
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=tokens))
