fix: Use attention mask for attention rnn seq2seq

seq2seq/layer.py
@@ -79,6 +79,8 @@ class BahdanauAttention(Layer):
     Call arguments:
         decoder_hidden: [BatchSize, HiddenDim]
         encoder_hiddens: [BatchSize, SequenceLength, HiddenDim]
+        mask: [BatchSize, SequenceLength]
+            Bool type tensor. The timesteps which has zero value will be ignored.
 
 
     Output Shape:
@@ -93,7 +95,7 @@ class BahdanauAttention(Layer):
         self.Ws = Dense(hidden_dim, name="value_converter")
         self.V = Dense(1, name="score")
 
-    def call(self, decoder_hidden: tf.Tensor, encoder_hiddens: tf.Tensor):
+    def call(self, decoder_hidden: tf.Tensor, encoder_hiddens: tf.Tensor, mask: tf.Tensor):
         # [BatchSize, HiddenDim]
         query = self.Wh(decoder_hidden)
         # [BatchSize, SequenceLength, HiddenDim]
@@ -101,6 +103,7 @@ class BahdanauAttention(Layer):
 
         # [BatchSize, SequenceLength, 1]
         score = self.V(tf.nn.tanh(tf.expand_dims(query, axis=1) + values))
+        score -= 1e9 * (1.0 - tf.cast(tf.expand_dims(mask, axis=2), tf.float32))
         attention = tf.nn.softmax(score, axis=1)
 
         # [BatchSize, HiddenDim]

seq2seq/model.py
@@ -297,7 +297,7 @@ class RNNSeq2SeqWithAttention(tf.keras.Model):
         decoder_output, *states = self.decoder[0](decoder_input, mask=decoder_mask, initial_state=states)
         decoder_mask = tf.concat([tf.ones([tf.shape(decoder_mask)[0], 1], tf.bool), decoder_mask], axis=1)
         for decoder_layer in self.decoder[1:]:
-            context = self.attention(states[0], encoder_input)[:, tf.newaxis, :]
+            context = self.attention(states[0], encoder_input, encoder_mask)[:, tf.newaxis, :]
             decoder_input = tf.concat([context, decoder_output], axis=1)
             decoder_output, *states = decoder_layer(decoder_input, mask=decoder_mask, initial_state=states)
             decoder_output = decoder_output[:, 1:, :]